95a6a0dd8d6fc3b8a03a73500bd5490868adc413
duyvu1109 <duyvu1109@gmail.com>
2021-10-24 18:25:37 +0700

    first

--- src/queue.c
+++ src/queue.c
@@ -8,12 +8,34 @@ int empty(struct queue_t * q) {
 
 void enqueue(struct queue_t * q, struct pcb_t * proc) {
 	/* TODO: put a new process to queue [q] */	
+	if (q->size == MAX_QUEUE_SIZE) {
+		printf("ENQUEUE fail!\n");
+		return;
+	}
+	q->proc[q->size] = proc;
+	q->size++;
+	printf("ENQUEUE successful!\n");
 }
 
 struct pcb_t * dequeue(struct queue_t * q) {
 	/* TODO: return a pcb whose prioprity is the highest
 	 * in the queue [q] and remember to remove it from q
 	 * */
-	return NULL;
+	if (q->size == 0) {
+		printf("DEQUEUE fail!\n");
+		return NULL;
+	}
+	int index = 0;
+	struct pcb_t *temp = q->proc[0];
+	for (int i = 0; i < q->size; i++) {
+		if (q->proc[i]->priority > temp->priority) {
+			index = i;
+			temp = q->proc[i];
+		}
+	}
+	q->proc[index] = NULL;
+	q->size--;
+	printf("DEQUEUE successful!\n");
+	return temp;
 }
 
